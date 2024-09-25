df159b21bab8fdd4d7c8abbb8ce166820ddb993c
Gabriel Racciatti <gabrielracciatti123@gmail.com>
2024-09-25 10:47:31 -0300

    Inicio api

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv').config()// Cargar variables de entorno al inicio
 const express = require('express')
-const empleadosController = require('./controllers/EmpleadosControllers')
+const empleadoRoutes = require('./routes/EmpleadoRoutes.js') // Importa el archivo de rutas
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use('/api/v1/empleados', empleadosController)
+app.use('/api/v1/Empleado', empleadoRoutes)// Usa el router en lugar del controlador
 
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`)
